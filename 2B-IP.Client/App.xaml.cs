cd91df710cb010eea12fef5ed87418975166a92d
Michael S. Kataev <lpestlname@gmail.com>
2025-08-09 01:51:39 +0300

    Added Client empty project

---
+++ 2B-IP.Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace _2B_IP.Client;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ 2B-IP.Client/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace _2B_IP.Client;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
